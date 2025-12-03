4099ef802e0844b73dc0e5d0622e71eeec77cd44
adzwar <adzwardwi111@gmail.com>
2025-12-03 22:23:05 +0700

    update feature multirole

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,23 @@
+<li class="sidebar-item active ">
+    <a href="index.html" class='sidebar-link'>
+        <i class="bi bi-grid-fill"></i>
+        <span>Admin</span>
+    </a>
+</li>
+<li class="sidebar-item  ">
+    <a href="#" class='sidebar-link'>
+        <i class="bi bi-grid-1x2-fill"></i>
+        <span>standalone menu</span>
+    </a>
+</li>
+<li class="sidebar-item  has-sub">
+    <a href="#" class='sidebar-link'>
+        <i class="bi bi-stack"></i>
+        <span>Menu with sub</span>
+    </a>
+    <ul class="submenu ">
+        <li class="submenu-item  ">
+            <a href="#" class="submenu-link">sub-menu</a>
+        </li>
+    </ul>
+</li>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -40,30 +40,19 @@
     </div>
     <div class="sidebar-menu">
         <ul class="menu">
-            <li class="sidebar-title">Menu</li>
-            <li class="sidebar-item active ">
-                <a href="index.html" class='sidebar-link'>
-                    <i class="bi bi-grid-fill"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li>
-            <li class="sidebar-item  ">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-grid-1x2-fill"></i>
-                    <span>standalone menu</span>
-                </a>
-            </li>
-            <li class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-stack"></i>
-                    <span>Menu with sub</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item  ">
-                        <a href="#" class="submenu-link">sub-menu</a>
-                    </li>
-                </ul>
-            </li>
+            <li class="sidebar-title">Menu {{ auth()->user()->role }}</li>
+
+            @if (auth()->user()->role === 'admin')
+                @include('admin.sidebar')
+            @endif
+
+            @if (auth()->user()->role === 'quality-checker')
+                @include('checker.sidebar')
+            @endif
+
+            @if (auth()->user()->role === 'pic')
+                @include('pic.sidebar')
+            @endif
         </ul>
     </div>
 </div>
